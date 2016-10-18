156bfdd5bd8d4198c848732536ffc05b7affbf5b
matiasev <evandromattias@gmail.com>
2016-10-18 12:41:45 -0200

    Shopping-List

---
+++ www/js/controller.js
@@ -0,0 +1,44 @@
+angular.module('meuApp', []);
+angular.module('meuApp').controller('meuController', function ($scope) {
+
+  var tasks = new getTasks();
+  
+  $scope.itens = tasks.produtos;
+  tasks.save();  
+  
+  $scope.numero = $scope.itens.length;
+  
+  
+  $scope.adicionarItem = function () {
+    $scope.errortext = "";
+
+    if (!$scope.adicionarText.nome) {
+      return;
+    }
+    if ($scope.itens.indexOf($scope.adicionarText.nome) == -1) {
+      
+      $scope.itens.push({nome:$scope.adicionarText.nome, done:false});
+      
+      
+      $scope.numero = $scope.itens.length;
+      
+      tasks.save();
+      
+    } else {
+      $scope.errortext = "Item já adicionado.";
+    }
+    delete $scope.adicionarText;
+  }
+  
+  $scope.removeItem = function (item) {
+    $scope.errortext = "";
+    $scope.itens.splice(item, 1);
+    $scope.numero = $scope.itens.length;
+    tasks.save();
+  };
+  $scope.selecionado = function(item){
+    item.done = !item.done;
+    tasks.save();
+  };
+  
+});
---
+++ www/js/model.js
@@ -0,0 +1,13 @@
+function getTasks() {
+  this.produtos = [];
+
+ var lista = localStorage.getItem("Shopping list");
+
+ if (lista !== null)
+  this.produtos = angular.fromJson(lista);
+
+  this.save = function() {
+    var lista = angular.toJson(this.produtos)
+    localStorage.setItem("Shopping list", lista);
+  }
+}
